1d651822257612a1e6399c19fac67cb5d37c5dea
Sam URIOSTEGUI <samyy.uv@gmail.com>
2022-09-26 15:41:48 +0200

    creating requests

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require("mongoose");
 require("dotenv").config();
+const userRoutes = require("./routes/user");
 
 const app = express();
 const port = process.env.PORT || 9000;
+
+//middleware
+app.use('/api', userRoutes);
 
 //routes
 app.get('/', (req, res) => {
