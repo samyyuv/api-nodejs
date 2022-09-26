618eecd3085e0cbfde09dc6aa01c2430eea6a0d1
Sam URIOSTEGUI <samyy.uv@gmail.com>
2022-09-26 16:17:32 +0200

    adding create and read endpoints

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const userSchema = require("../models/user");
 const router = express.Router();
 
 // create user
 router.post('/users', (req, res) => {
-  res.send('create user')
+  const user = userSchema(req.body)
+  user
+  .save()
+  .then((data) => res.json(data))
+  .catch((error) => res.json({ message: error}))
+});
+
+// create user
+router.get('/users', (req, res) => {
+  userSchema
+  .find()
+  .then((data) => res.json(data))
+  .catch((error) => res.json({ message: error}))
 });
 
 module.exports = router;
